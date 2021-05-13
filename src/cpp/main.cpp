8ec5f64dbb5f8f0e9c914342df152d4fcb4b5a50
Garen Wang <garen-wang@qq.com>
2021-05-13 17:06:29 +0800

    support read code from file

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,7 +1,10 @@
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
 #include "parser.cpp"
 
 Parser generate() {
@@ -10,15 +13,25 @@ Parser generate() {
   return g;
 }
 
-void io(Parser &g) {
+void io(Parser &g, const std::string &inputFile) {
   std::string str, input;
-  while (true) {
-    std::getline(std::cin, str);
-    if (str == "q" || str == "quit") break;
-    // WARNING: check input string to prevent injection attack
-    if (str != "") input += str + '\n';
-    else break;
+  if (!inputFile.empty()) {
+    std::ifstream ifs(inputFile);
+    std::ostringstream temp;
+    temp << ifs.rdbuf();
+    input = temp.str();
+    input += '\n';
+  } else {
+    while (true) {
+      std::getline(std::cin, str);
+      if (str == "q" || str == "quit") break;
+      // WARNING: check input string to prevent injection attack
+      if (str != "") input += str + '\n';
+      else break;
+    }
   }
+  // DEBUG
+  // std::cout << input << std::endl;
   try {
     auto output = g.run(input);
     std::cout << "Parsing result: " << output << std::endl;
@@ -28,8 +41,16 @@ void io(Parser &g) {
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
+  std::string inputFile;
+  for (int i = 1; i < argc; ++i) {
+    // std::cout << argv[i] << std::endl;
+    if (!strcmp(argv[i], "-f")) {
+      if (i + 1 < argc) inputFile = argv[i + 1];
+      else throw std::invalid_argument("format error");
+    }
+  }
   auto g = generate();
-  io(g);
+  io(g, inputFile);
   return 0;
 }
