b843d5e62d93d8e2c4827233ba193b5a0a773aa3
Garen Wang <garen-wang@qq.com>
2021-05-30 20:36:53 +0800

    finished test

--- src/shl/main.cpp
+++ src/shl/main.cpp
@@ -14,29 +14,91 @@ int main(int argc, char **argv) {
       else throw std::invalid_argument("Arguments format error.");
     }
   }
+  testFileName = "../src/python/tests/test.py";
+  std::ifstream ifs(testFileName);
+  std::string testInput((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
 //  auto result = generateParserFromSHL(shlFileName);
 
+  /*
   auto result = generateParserFromSHL("../src/python/python3.shl");
   auto gen = result.second;
-  gen["Program"] << "Statement | Expr";
+
+
+
+  std::vector<int> indentDepth;
+  std::vector<std::pair<long, long> > records;
+  gen["InitBlocks"] << "''" << [&](auto&) -> bool {
+    indentDepth.resize(0);
+    return true;
+  } >> [](auto) { return "InitBlocks"; };
+  gen["SameIndentation"] << "Indentation" << [&](auto &s) -> bool {
+    return s->length() == indentDepth.back();
+  } >> [](auto) { return "SameIndentation"; };
+  gen["SameIndentation"]->cacheable = false;
+
+  gen["DeepIndentation"] << "Indentation" << [&](auto &s) -> bool {
+    return s->length() > indentDepth.back();
+  } >> [](auto) { return "DeepIndentation"; };
+  gen["DeepIndentation"]->cacheable = false;
+
+  gen["EnterBlock"] << "Indentation" << [&](auto &s) -> bool {
+    if (indentDepth.empty() || s->length() > indentDepth.back()) {
+      indentDepth.push_back(s->length());
+      return true;
+    } else return false;
+  } >> [](auto) { return "EnterBlock"; };
+  gen["EnterBlock"]->cacheable = false;
+
+  gen["Line"] << "SameIndentation Grammar '\n'" >> [](auto) { return "Line"; };
+  gen.getRule("Line")->cacheable = false;
+
+  gen["EmptyLine"] << "Indentation '\n'" >> [](auto) { return "EmptyLine"; };
+
+  gen["ExitBlock"] << "''" << [&](auto&) -> bool {
+    indentDepth.pop_back();
+    return true;
+  } >> [](auto) { return "ExitBlock"; };
+  gen.getRule("ExitBlock")->cacheable = false;
+
+  gen["Block"] << "&EnterBlock Line (EmptyLine | Block | Line)* &ExitBlock" >> [&](auto e) {
+    for (auto x : e) x.evaluate();
+//    blocks.push_back(Block{e.position(), e.length()});
+//    std::cout << "position: from " << e.position() << " to " << e.position() + e.length() << std::endl;
+    auto temp = getLineNumber(testInput, e.position(), e.length());
+    records.push_back(temp);
+//    std::cout << "line from " << temp.first << " to " << temp.second << std::endl;
+    return "Block";
+  };
+
+  gen["Grammar"] << "(Heads | Statement | Expr)";
+
+  gen["Start"] << "InitBlocks (ImportStatement '\n')* Block";
+  gen.setStart(gen["Start"]);
+  */
+
+
+
+
+//  gen["Program"] << "Statement | Expr";
 //  gen["Program"] << "DefHead";
-  gen.setStart(gen["Program"]);
+//  gen.setStart(gen["Program"]);
 
-//  auto temp = generateLanguageParser(LanguageType::JAVA);
-//  assert(temp.first);
-//  auto gen = temp.second;
+  auto temp = generateLanguageParser(LanguageType::PYTHON);
+  assert(temp.first);
+  auto gen = temp.second;
 
-  testFileName = "../src/python/tests/test.py";
-//  testFileName = "../src/java/tests/test.java";
-  if (!testFileName.empty()) {
-    std::ifstream ifs(testFileName);
-    std::string testInput((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-    try {
-      auto testOutput = gen.run(testInput);
-      std::cout << "Parsing result: " << testOutput << std::endl;
-    } catch (peg_parser::SyntaxError &e) {
-      std::cout << "Syntax error when parsing " << e.syntax->rule->name << std::endl;
+  //  testFileName = "../src/java/tests/test.java";
+  try {
+    auto testOutput = gen.run(testInput);
+    std::cout << "Parsing result: " << testOutput << std::endl;
+    std::cout << blockRecords.size() << std::endl;
+    for (auto x : blockRecords) {
+      std::cout << x.first << ", " << x.second << std::endl;
+      auto t = getLineNumber(testInput, x);
+      std::cout << t.first << ", " << t.second << std::endl;
     }
+  } catch (peg_parser::SyntaxError &e) {
+    std::cout << "Syntax error when parsing " << e.syntax->rule->name << std::endl;
   }
   return 0;
 }
