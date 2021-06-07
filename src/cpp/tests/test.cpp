f06d0953079a86ec6363491f199554a5ea260736
Garen Wang <garen-wang@qq.com>
2021-06-07 23:59:45 +0800

    work done against test code

--- src/cpp/tests/test.cpp
+++ src/cpp/tests/test.cpp
@@ -98,7 +98,7 @@ int main() {
 }
 
 // return a random integer in the range `[0,' `n)'
-int nrand(int n) {
+std::size_t nrand(int n) {
   if (n <= 0 || n > RAND_MAX) throw domain_error("Argument to nrand is out of range");
 
   const int bucket_size = RAND_MAX / n;
