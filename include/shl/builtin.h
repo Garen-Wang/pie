30412d9fdb9a929bd54e02dd230b1826a64c85fe
Garen Wang <garen-wang@qq.com>
2021-06-14 23:31:15 +0800

    reformatted code written by wy

--- include/shl/builtin.h
+++ include/shl/builtin.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SHL_BUILTIN_H
+#define SHL_BUILTIN_H
 
 #include <peg_parser/generator.h>
 
@@ -19,59 +20,57 @@ using Attr = Style;
 
 namespace shl {
     enum class LanguageType {
-      CPP,
-      JAVA,
-      PYTHON,
-      JSON,
+        CPP,
+        JAVA,
+        PYTHON,
     };
 
-  extern std::set<std::string> identifiers;
+    class SyntaxHighlightInfo {
+      public:
+        mutable int idx = 0;
+        Attr attr;
+        explicit SyntaxHighlightInfo(Attr attr);
+        explicit SyntaxHighlightInfo(int idx, Attr attr);
+    };
 
-  class SyntaxHighlightInfo {
-  public:
-    mutable int idx = 0;
-    Attr attr;
-    explicit SyntaxHighlightInfo(Attr attr);
-    explicit SyntaxHighlightInfo(int idx, Attr attr);
-  };
+    typedef std::vector<SyntaxHighlightInfo> SyntaxHighlightInfos;
+    using Parser = peg_parser::ParserGenerator<std::string>;
+    using ParserBuilder = peg_parser::ParserGenerator<std::shared_ptr<SyntaxHighlightInfos>, Parser &>;
 
-  typedef std::vector<SyntaxHighlightInfo> SyntaxHighlightInfos;
-  using Parser = peg_parser::ParserGenerator<std::string>;
-  using ParserBuilder = peg_parser::ParserGenerator<std::shared_ptr<SyntaxHighlightInfos>, Parser&>;
+    class Colors {
+      private:
+        // not wise to use *unordered_set* here, try *unordered_map*
+        std::unordered_map<std::string, QColor> colors;
 
-  class Colors {
-  private:
-    // not wise to use *unordered_set* here, try *unordered_map*
-    std::unordered_map<std::string, QColor> colors;
+      public:
+        // get expression for PEG parser grammar
+        std::string getExpr();
+        // hope all string in `colors` are lowercase
+        void append(const std::string &color, const QColor &_color);
+        bool exist(const std::string &str);
+        QColor get(const std::string &str);
+    };
 
-  public:
-    // get expression for PEG parser grammar
-    std::string getExpr();
-    // hope all string in `colors` are lowercase
-    void append(const std::string& color, const QColor& _color);
-    bool exist(const std::string& str);
-    QColor get(const std::string& str);
-  };
+    ParserBuilder *generateParserBuilder(Colors &colors, Rope *);
 
-  ParserBuilder *generateParserBuilder(Colors& colors, Rope *);
+    Colors getPredefinedColors();
 
-  Colors getPredefinedColors();
+    std::pair<bool, Parser> generateParserFromSHL(const std::string &filename, Rope *rope);
 
-  std::pair<bool, Parser> generateParserFromSHL(const std::string& filename, Rope *rope);
+    void initParserBuilder(ParserBuilder &g, Colors &colors, Rope *, bool render = false);
 
-  bool renderSyntaxHighlightForSHL(const std::string& filename);
+    extern std::vector<int> indentDepth;
+    extern std::vector<std::pair<int, int>> blockRecords;
 
-  void initParserBuilder(ParserBuilder& g, Colors& colors, Rope *, bool render = false);
+    void initSHLGrammar(ParserBuilder &g);
 
-  extern std::vector<int> indentDepth;
-  extern std::vector<std::pair<int, int> > blockRecords;
+    std::pair<long, long> getLineNumber(std::string_view input, std::pair<int, int> blockRecord);
 
-  void initSHLGrammar(ParserBuilder &g);
+    int getIdx(const std::string &str, int size);
 
-  std::pair<long, long> getLineNumber(std::string_view input, std::pair<int, int> blockRecord);
+    bool isLowercase(const std::string &str);
 
-  int getIdx(const std::string& str, int size);
+    std::pair<bool, shl::Parser> generateLanguageParser(LanguageType languageType, Rope *);
+}// namespace shl
 
-  bool isLowercase(const std::string& str);
-  std::pair<bool, shl::Parser> generateLanguageParser(LanguageType languageType, Rope *);
-}  // namespace shl
+#endif// SHL_BUILTIN_H
--- src/shl/parser_builder.cpp
+++ src/shl/parser_builder.cpp
@@ -1,78 +1,81 @@
-//
-// Created by garen on 5/29/21.
-//
+/* The methods defined here are used to generate a *ParserBuilder*
+ * and the way to generate *Parser* through generated *ParserBuilder*.
+ */
 
 #include "builtin.h"
 
 namespace shl {
-  std::set<std::string> identifiers;
-  SyntaxHighlightInfo::SyntaxHighlightInfo(Attr attr) {
-    this->idx = -1;
-    this->attr = attr;
-  }
-
-  SyntaxHighlightInfo::SyntaxHighlightInfo(int idx, Attr attr) {
-    this->idx = idx;
-    this->attr = attr;
-  }
-
-  std::string Colors::getExpr() {
-    std::string expr = "(";
-    for (const auto& it : colors) {
-      expr += "'" + it.first + "'|";
+    SyntaxHighlightInfo::SyntaxHighlightInfo(Attr attr) {
+        this->idx = -1;
+        this->attr = attr;
     }
-    expr.pop_back();
-    expr += ")";
-    return expr;
-  }
 
-  void Colors::append(const std::string& color, const QColor& _color) { colors[color] = _color; }
-  bool Colors::exist(const std::string& str) { return colors.count(str) != 0; }
-  QColor Colors::get(const std::string& str) { return colors[str]; }
-
-  ParserBuilder *generateParserBuilder(Colors& colors, Rope *rope) {
-    ParserBuilder *g = new ParserBuilder();
-    initParserBuilder(*g, colors, rope);
-    return g;
-  }
-
-  Colors getPredefinedColors() {
-    Colors colors;
-    colors.append("black", QColor(0, 0, 0));
-    colors.append("gray", QColor(80, 80, 80));
-    colors.append("shallow_yellow", QColor(204, 255, 0));
-    colors.append("blue", QColor(0, 0, 255));
-    colors.append("cyan", QColor(0, 255, 255));
-    colors.append("purple", QColor(127, 0, 255));
-    colors.append("green", QColor(0, 128, 0));
-    colors.append("dark_purple", QColor(80, 80, 120));
-    colors.append("orange", QColor(255, 20, 147));
-    colors.append("white", QColor(255, 255, 255));
-    colors.append("yellow", QColor(255, 255, 0));
-    colors.append("pink", QColor(255, 182, 193));
-    return colors;
-  }
-
-  std::pair<bool, Parser> generateParserFromSHL(const std::string& filename, Rope *rope) {
-    Colors colors = getPredefinedColors();
-    ParserBuilder *g = generateParserBuilder(colors, rope);
-    std::ifstream ifs(filename);
-    std::string shlFile((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-    Parser gen;
-    bool flag = true;
-    try {
-      auto output = g->run(shlFile, gen);
-//      gen["Identifier"] >> [](auto) {
-      //        std::cout << "11" << std::endl;
-      //        return "Identifier";
-      //      };
-      //    std::cout << "Parsing result: " << output << std::endl;
-    } catch (peg_parser::SyntaxError& e) {
-      flag = false;
-      std::cout << "ParserBuilder: Syntax error when parsing " << e.syntax->rule->name << std::endl;
+    SyntaxHighlightInfo::SyntaxHighlightInfo(int idx, Attr attr) {
+        this->idx = idx;
+        this->attr = attr;
     }
-    return std::make_pair(flag, gen);
-  }
 
-  bool renderSyntaxHighlightForSHL(const std::string& filename) { return false; }
-}  // namespace shl
+    std::string Colors::getExpr() {
+        std::string expr = "(";
+        for (const auto &it : colors) {
+            expr += "'" + it.first + "'|";
+        }
+        expr.pop_back();
+        expr += ")";
+        return expr;
+    }
+
+    void Colors::append(const std::string &color, const QColor &_color) {
+        colors[color] = _color;
+    }
+    bool Colors::exist(const std::string &str) {
+        return colors.count(str) != 0;
+    }
+    QColor Colors::get(const std::string &str) {
+        return colors[str];
+    }
+
+    ParserBuilder *generateParserBuilder(Colors &colors, Rope *rope) {
+        auto *g = new ParserBuilder();
+        initParserBuilder(*g, colors, rope);
+        return g;
+    }
+
+    Colors getPredefinedColors() {
+        Colors colors;
+        colors.append("black", QColor(0, 0, 0));
+        colors.append("gray", QColor(80, 80, 80));
+        colors.append("shallow_yellow", QColor(204, 255, 0));
+        colors.append("blue", QColor(0, 0, 255));
+        colors.append("cyan", QColor(0, 255, 255));
+        colors.append("purple", QColor(127, 0, 255));
+        colors.append("green", QColor(0, 128, 0));
+        colors.append("dark_purple", QColor(80, 80, 120));
+        colors.append("orange", QColor(255, 20, 147));
+        colors.append("white", QColor(255, 255, 255));
+        colors.append("yellow", QColor(255, 255, 0));
+        colors.append("pink", QColor(255, 182, 193));
+        return colors;
+    }
+
+    std::pair<bool, Parser> generateParserFromSHL(const std::string &filename, Rope *rope) {
+        Colors colors = getPredefinedColors();
+        ParserBuilder *g = generateParserBuilder(colors, rope);
+        std::ifstream ifs(filename);
+        std::string shlFile((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+        Parser gen;
+        bool flag = true;
+        try {
+            auto output = g->run(shlFile, gen);
+            // gen["Identifier"] >> [](auto) {
+            //        std::cout << "11" << std::endl;
+            //        return "Identifier";
+            //      };
+            //    std::cout << "Parsing result: " << output << std::endl;
+        } catch (peg_parser::SyntaxError &e) {
+            flag = false;
+            std::cout << "ParserBuilder: Syntax error when parsing " << e.syntax->rule->name << std::endl;
+        }
+        return std::make_pair(flag, gen);
+    }
+}// namespace shl
